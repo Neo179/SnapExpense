b2668592fd321a85d5fbeb7bb61663c89cb3e64b
Saurabh Padhi <saurabh.or.padhi@gmail.com>
2026-05-14 12:20:13 +0530

    Fixed deployment issues

--- configweb/configweb/settings.py
+++ configweb/configweb/settings.py
@@ -126,3 +126,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
